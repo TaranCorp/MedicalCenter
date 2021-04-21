54970d595ffd31d2985b1fbd9c135e5dafb7d262
maciekbulanda <bma@post.pl>
2021-04-21 13:46:15 +0200

    Added main class MedicalCenterApplication

---
+++ src/main/java/com/javateam/medicalcenter/MedicalCenterApplication.java
@@ -0,0 +1,11 @@
+package com.javateam.medicalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCenterApplication.class, args);
+    }
+}
