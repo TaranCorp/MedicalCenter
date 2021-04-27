62f36a6e499e3a6e433205e3a9dc13a9e9d08c0a
OlgaOpalinska <olgaopalinska@wp.pl>
2021-04-27 18:19:16 +0200

    Create necessary User fields

--- src/main/java/com/javateam/medicalcenter/entity/User.java
+++ src/main/java/com/javateam/medicalcenter/entity/User.java
@@ -4,17 +4,38 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.sql.Time;
 
 @Entity
 @Table(name="USERS")
 @Inheritance(strategy= InheritanceType.JOINED)
 @DiscriminatorColumn(name = "USER_TYPE", discriminatorType = DiscriminatorType.STRING)
-// W dziedziczących klasach dodajemy @DiscrimunatorValue("nazwa_typu_usera) nad każdą klasą
+// W dziedziczących klasach dodajemy @DiscriminatorValue("nazwa_typu_usera) nad każdą klasą
 @Data
 @NoArgsConstructor
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     private Long user_id;
+    @Column(name = "LOGIN")
+    private String login;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "FIRST_NAME")
+    private String name;
+    @Column(name = "FAMILY_NAME")
+    private String surname;
+    @Column(name = "PESEL")
+    private Long pesel;
+    @Column(name = "DATE_OF_BIRTH")
+    private Time dateOfBirth;
+    @Column(name = "SEX")
+    private boolean isMale;
+    @Column(name = "MAIL")
+    private String email;
 
+//    @OneToMany(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "USER_ID")
+//    private List<Appointment> appointments;
 }
