b103cdc0364f0d1a21424f5aaf52fea352abf0c1
OlgaOpalinska <olgaopalinska@wp.pl>
2021-04-27 17:44:08 +0200

    Fix db logging + lombok problem (somehow)

--- src/main/java/com/javateam/medicalcenter/entity/User.java
+++ src/main/java/com/javateam/medicalcenter/entity/User.java
@@ -1,5 +1,8 @@
 package com.javateam.medicalcenter.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
@@ -7,6 +10,8 @@ import javax.persistence.*;
 @Inheritance(strategy= InheritanceType.JOINED)
 @DiscriminatorColumn(name = "USER_TYPE", discriminatorType = DiscriminatorType.STRING)
 // W dziedziczących klasach dodajemy @DiscrimunatorValue("nazwa_typu_usera) nad każdą klasą
+@Data
+@NoArgsConstructor
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
