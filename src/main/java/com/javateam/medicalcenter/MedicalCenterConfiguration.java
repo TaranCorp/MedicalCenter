1f83cf174ebb5947d8814f69f4f63b89ec828a82
Alexei A <alexey.alexandrenko@gmail.com>
2021-05-05 09:30:35 +0200

    Adds architecture folder structure example

---
+++ src/main/java/com/javateam/medicalcenter/MedicalCenterConfiguration.java
@@ -0,0 +1,14 @@
+package com.javateam.medicalcenter;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MedicalCenterConfigurationProperties.class)
+public class MedicalCenterConfiguration {
+
+
+  }
+
+}
---
+++ src/main/java/com/javateam/medicalcenter/account/application/service/NoOpAccountLock.java
@@ -0,0 +1,20 @@
+package com.javateam.medicalcenter.account.application.service;
+
+import io.reflectoring.buckpal.account.application.port.out.AccountLock;
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import org.springframework.stereotype.Component;
+
+@Component
+class NoOpAccountLock implements AccountLock {
+
+	@Override
+	public void lockAccount(AccountId accountId) {
+		// do nothing
+	}
+
+	@Override
+	public void releaseAccount(AccountId accountId) {
+		// do nothing
+	}
+
+}
