f139ebed85c776dc213e7c65faa9bb8dbb9d4a86
OlgaOpalinska <olgaopalinska@wp.pl>
2021-05-25 16:34:33 +0200

    configuration properties & user id fix (Hibernate didn't understand)

--- src/main/java/com/javateam/medicalcenter/users/domain/User.java
+++ src/main/java/com/javateam/medicalcenter/users/domain/User.java
@@ -3,7 +3,6 @@ package com.javateam.medicalcenter.users.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.math3.random.RandomDataGenerator;
 
 import javax.persistence.*;
 
@@ -19,15 +18,7 @@ public abstract class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
-
-    long leftLimit = 1000000L;
-    long rightLimit = 3000000L;
-    long id = new RandomDataGenerator().nextLong(leftLimit, rightLimit);
-
-    public Long getId () {
-        return id;
-    }
+    private Long id;
 
     @Column(name = "LOGIN")
     private String login;
