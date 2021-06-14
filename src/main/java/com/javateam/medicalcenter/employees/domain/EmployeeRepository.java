e2fced57a35c41e3cab8ecc04237030395be8889
OlgaOpalinska <olgaopalinska@wp.pl>
2021-06-14 14:16:04 +0200

    Delete repositories and services for abstract classes

--- src/main/java/com/javateam/medicalcenter/employees/domain/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.javateam.medicalcenter.employees.domain;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository <Employee, Long> {
-
-}
