af712603c47a49bc2016be9d29186fe5ebfbd329
Alexei A <alexey.alexandrenko@gmail.com>
2021-08-07 15:47:19 +0200

    Typo fix. Now tables are being generated upon a start.

--- src/main/java/com/javateam/medicalcenter/MedicalCenterApplication.java
+++ src/main/java/com/javateam/medicalcenter/MedicalCenterApplication.java
@@ -1,5 +1,7 @@
 package com.javateam.medicalcenter;
 
+import com.javateam.medicalcenter.appointments.domain.Appointment;
+import com.javateam.medicalcenter.patients.domain.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +11,13 @@ public class MedicalCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(MedicalCenterApplication.class, args);
 
+        Patient patient = new Patient();
+        Appointment appointment = new Appointment();
+
         Util util = new Util();
-        util.getConnection();
+        util.getConnection("ss","ss");
+
+
 
 
     }
